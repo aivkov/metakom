24e78cccd517ff8daf3bebbd3dffd868f34701d6
Alexey <a343147@yandex.ru>
2025-06-22 23:28:24 +0300

    Prepare directories

--- poverka-37.ru/public_html/index.php
+++ poverka-37.ru/public_html/index.php
@@ -1,4 +1,12 @@
-<?php 
-require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-echo SITE_ID;
-?>
+<?php
+/** @var \CMain $APPLICATION */
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+
+$APPLICATION->SetPageProperty("description", "Компания &quot;Метаком-Сервис&quot; занимается поверкой приборов учёта водоснабжения в городе Иваново. Внесение данных в систему &quot;АРШИН&quot;");
+$APPLICATION->SetPageProperty("title", "Поверка счётчиков воды | Компания Метаком Иваново");
+$APPLICATION->SetTitle("Поверка приборов учета водоснабжения");?>
+
+Поверка счетчиков воды Иваново
+
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
