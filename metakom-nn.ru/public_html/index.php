c77d5dd6f178accb28b30df84c2de1b587db617c
Alexey <a343147@yandex.ru>
2025-06-28 15:36:51 +0300

    add metakom-nn site

---
+++ metakom-nn.ru/public_html/index.php
@@ -0,0 +1,13 @@
+<?php
+/** @var \CMain $APPLICATION */
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+
+$APPLICATION->SetPageProperty("description", "ООО «Метаком Сервис» - Обслуживание, монтаж и ремонт домофонного оборудования, систем видеонаблюдения, СКУД г. Нижний Новгород");
+$APPLICATION->SetPageProperty("keywords", "Метаком Сервис, обслуживание, монтаж и ремонт домофонов, систем видеонаблюдения, СКУД,  г. Нижний Новгород");
+$APPLICATION->SetPageProperty("title", "Поверка счётчиков воды | Компания Метаком Иваново");
+$APPLICATION->SetTitle("Поверка приборов учета водоснабжения");?>
+
+Метаком Нижний Новгород
+
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
