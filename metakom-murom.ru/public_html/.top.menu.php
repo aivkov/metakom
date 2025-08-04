754c0b2485ad9a87a2797604a54b19efa83bf29a
Alexey <a343147@yandex.ru>
2025-08-04 23:05:37 +0300

    add murom metakom site

---
+++ metakom-murom.ru/public_html/.top.menu.php
@@ -0,0 +1,46 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Новости",
+		"/news/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"О нас",
+		"/about/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Услуги",
+		"/services/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+    Array(
+        "Поверка счетчиков воды",
+        "https://poverka-37.ru",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Оформить заявку",
+        "#",
+        Array(),
+        Array(),
+        ""
+    ),
+	Array(
+		"Контакты", 
+		"/contacts/",
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ metakom-murom.ru/public_html/index.php
@@ -0,0 +1,22 @@
+<?php
+/** @var \CMain $APPLICATION */
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+
+$APPLICATION->SetPageProperty("description", "ООО «Метаком Сервис» - Обслуживание, монтаж и ремонт домофонного оборудования, систем видеонаблюдения, СКУД г. Муром");
+$APPLICATION->SetPageProperty("keywords", "Метаком Сервис, обслуживание, монтаж и ремонт домофонов, систем видеонаблюдения, СКУД,  г. Муром");
+$APPLICATION->SetPageProperty("title", "ООО «Метаком Сервис» - Обслуживание, монтаж и ремонт домофонного оборудования, систем видеонаблюдения, СКУД г. Муром");
+$APPLICATION->SetTitle("Метаком Сервис Муром");?>
+
+<div class="container">
+    <div class="section">
+        <?php $APPLICATION->IncludeFile('/includes/metakom/banner.php', ['ID' => 9])?>
+    </div>
+    <div class="section">
+        <?php $APPLICATION->IncludeFile('/includes/metakom/advantages.php')?>
+    </div>
+</div>
+
+<?php $APPLICATION->IncludeFile('/includes/metakom/services.php', ['ID' => 11])?>
+
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
