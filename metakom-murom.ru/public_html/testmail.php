10adeec94a68e8ef168e985662baf306133ce637
Alexey <a343147@yandex.ru>
2025-08-10 12:36:26 +0300

    test mail

--- metakom-murom.ru/public_html/testmail.php
+++ metakom-murom.ru/public_html/testmail.php
@@ -5,7 +5,7 @@ $subject = 'Тема письмо';
 $message = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><title>Рассылка CMD</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></head><body>';
 $message .= '<p>Проверка связи</p>';
 $message .= '</body></html>';
-$headers = 'From: Конпания Метаком Сервис<no-reply@metakom-murom.ru>' . "\r\n" .
+$headers = 'From: no-reply@metakom-murom.ru' . "\r\n" .
     'MIME-Version: 1.0' . "\r\n" .
     'Content-type: text/html; charset=utf-8' . "\r\n";
 
