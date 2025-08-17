2ae7b9d56fb0d20518c4088b07c8460afd7f2bb1
Alexey <a343147@yandex.ru>
2025-08-17 20:42:52 +0300

    start make poverka-52 site

--- poverka-52.ru/public_html/index.php
+++ poverka-52.ru/public_html/index.php
@@ -1,1 +1,21 @@
-<?php echo 123;
+<?php
+/** @var \CMain $APPLICATION */
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+
+$APPLICATION->SetPageProperty("description", "ООО «Метаком Сервис» - Обслуживание, монтаж и ремонт домофонного оборудования, систем видеонаблюдения, СКУД г. Муром");
+$APPLICATION->SetPageProperty("keywords", "Метаком Сервис, обслуживание, монтаж и ремонт домофонов, систем видеонаблюдения, СКУД,  г. Муром");
+$APPLICATION->SetPageProperty("title", "ООО «Метаком Сервис» - Обслуживание, монтаж и ремонт домофонного оборудования, систем видеонаблюдения, СКУД г. Муром");
+$APPLICATION->SetTitle("Метаком Сервис Муром");
+
+$APPLICATION->SetAdditionalCss(CUtil::GetAdditionalFileURL('/local/css/banner.css'));
+?>
+
+    <div class="container">
+        <div class="section">
+            <h2 class="section__title">Контакты</h2>
+            <?php $APPLICATION->IncludeFile('/includes/metakom/contacts.php') ?>
+        </div>
+    </div>
+
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
