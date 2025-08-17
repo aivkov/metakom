86c4fad17fb5d7c8fb669c37f83a1a740537761d
Alexey <a343147@yandex.ru>
2025-08-17 20:17:22 +0300

    create poverka-52 site

--- metakom-murom.ru/public_html/testmail.php
+++ metakom-murom.ru/public_html/testmail.php
@@ -1,7 +1,7 @@
 <?php
 
 $to      = 'a343147@yandex.ru';
-$subject = 'Тема письмо';
+$subject = 'Новая заявка';
 $message = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><title>Рассылка CMD</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></head><body>';
 $message .= '<p>Проверка связи</p>';
 $message .= '</body></html>';
