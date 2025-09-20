6f7a539bcca4944ed3e8da0732a456c56c7d2b48
Alexey <a343147@yandex.ru>
2025-09-20 19:50:32 +0300

    add metakom-vz.ru site

---
+++ metakom-vz.ru/public_html/.top.menu.php
@@ -0,0 +1,40 @@
+<?
+$aMenuLinks = Array(
+    Array(
+        "Для чего нужна поверка",
+        "/dlya_chego_poverka_schetchikov/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Как защититься от мошейников",
+        "/kak_izbejat/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Аттестат аккредитации",
+        "/accred/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Оферта",
+        "/docs/Оферта_о_присоединении_к_услугам_поверка_замена_Робокасса_март_2025.docx",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Контакты",
+        "/contacts/",
+        Array(),
+        Array(),
+        ""
+    ),
+
+);
+?>
---
+++ metakom-vz.ru/public_html/index.php
@@ -0,0 +1,36 @@
+<?php
+/** @var \CMain $APPLICATION */
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+
+$APPLICATION->SetPageProperty("description", "Компания «Метаком-Сервис» занимается поверкой счетчиков воды в Нижнем Новгороде. Внесение данных в систему «АРШИН»");
+$APPLICATION->SetPageProperty("keywords", "Поверка счетчиков воды, Метаком Сервис Нижний Новгород");
+$APPLICATION->SetPageProperty("title", "Поверка счетчиков воды | Компания Метаком Сервис Нижний Новгород");
+$APPLICATION->SetTitle("Поверка счетчиков воды, Метаком Сервис Нижний Новгород");
+
+$APPLICATION->SetAdditionalCss(CUtil::GetAdditionalFileURL('/local/css/banner.css'));
+?>
+    <div class="container">
+        <div class="section section--no-pt section--no-pb">
+            <?php $APPLICATION->IncludeFile('/includes/metakom/news-detail.php', ['CODE' => 'main-banner', 'TEMPLATE' => 'banner']) ?>
+        </div>
+
+        <div class="section">
+            <?php $APPLICATION->IncludeFile('/includes/metakom/news-detail.php', ['CODE' => 'main-about', 'TEMPLATE' => 'content']) ?>
+        </div>
+
+        <div class="section section--no-pt">
+            <?php $APPLICATION->IncludeFile('/includes/metakom/news-list.php', ['SECTION_CODE' => 'advantages', 'TEMPLATE' => 'advantages']) ?>
+        </div>
+    </div>
+
+<?php $APPLICATION->IncludeFile('/includes/metakom/news-list.php', ['SECTION_CODE' => 'services', 'TEMPLATE' => 'services']) ?>
+
+    <div class="container">
+        <div class="section">
+            <h2 class="section__title">Контакты</h2>
+            <?php $APPLICATION->IncludeFile('/includes/metakom/contacts.php') ?>
+        </div>
+    </div>
+
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
