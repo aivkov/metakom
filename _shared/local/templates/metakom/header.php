8af56f7ee6de458822ebd8631f106353deec8878
Alexey <a343147@yandex.ru>
2025-10-12 16:54:37 +0300

    add favicons

--- _shared/local/templates/metakom/header.php
+++ _shared/local/templates/metakom/header.php
@@ -17,6 +17,17 @@ $assets->addCss('/local/css/main.css');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/favicon.ico')):?>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <?php endif?>
+    <?php if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/favicon-32.png')):?>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">
+    <?php endif?>
+    <?php if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/favicon-16.png')):?>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">
+    <?php endif?>
+
     <title><?php $APPLICATION->ShowTitle(); ?></title>
     <?=Site::getGoogleVerification()?>
     <?=Site::getYandexVerification()?>
