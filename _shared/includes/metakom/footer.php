574ee451872ec39c246c538d7a909656e5817241
Alexey <a343147@yandex.ru>
2026-02-17 11:31:35 +0300

    cout an oferta krasnodar

--- _shared/includes/metakom/footer.php
+++ _shared/includes/metakom/footer.php
@@ -13,8 +13,13 @@ $arLogo = Site::getLogo();
 
             <?php $APPLICATION->IncludeFile('/includes/metakom/social.php')?>
         </div>
-        <div class="footer__copy">
-            © <?=date('Y')?> Сайт является информационным ресурсом <?=Site::getSiteName()?>
+        <div class="footer__bottom">
+            <div class="footer__copy">
+                © <?=date('Y')?> Сайт является информационным ресурсом <?=Site::getSiteName()?>
+            </div>
+            <div class="footer__disclamer">Вся представленная на сайте информация носит исключительно информационный характер,
+                и ни при каких условиях не является публичной офертой, определяемой положениями Статьи 437
+                Гражданского кодекса Российской Федерации.</div>
         </div>
     </div>
 </footer>
---
+++ metakom-krasnodar.ru/public_html/sout/index.php
@@ -0,0 +1,16 @@
+<?php
+/** @var \CMain $APPLICATION */
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+
+$APPLICATION->SetTitle("СОУТ");
+$APPLICATION->SetPageProperty("title", "Специальная оценка условий труда | Краснодар");
+?>
+
+    <div class="page">
+        <div class="container">
+            <h1 class="page__title"><?php $APPLICATION->ShowTitle(false) ?></h1>
+            <?php $APPLICATION->IncludeFile('/includes/metakom/news-detail.php', ['CODE' => 'sout', 'TEMPLATE' => 'content']) ?>
+        </div>
+    </div>
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
