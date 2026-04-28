956e073f0f1e737a9ec84e2fbf1efbc0e2588c69
Alexey <a343147@yandex.ru>
2026-04-28 19:04:50 +0300

    fix ermitage mobis steps

--- _shared/local/templates/mobis/components/bitrix/news.list/steps/template.php
+++ _shared/local/templates/mobis/components/bitrix/news.list/steps/template.php
@@ -1,4 +1,4 @@
-<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) {
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
@@ -17,29 +17,30 @@
 
 ?>
 
-<?php if($arResult['ITEMS']):?>
+<?php if ($arResult['ITEMS']): ?>
     <div class="steps">
         <div class="steps__list">
-            <?php foreach($arResult['ITEMS'] as $key => $arItem):?>
+            <?php foreach ($arResult['ITEMS'] as $key => $arItem): ?>
                 <?php
                 $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                 $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
                 ?>
-                    <div class="steps__img" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-                        <?php $arSmallFile = CFile::ResizeImageGet($arItem['PROPERTIES']['PICTURE']['VALUE'], ['width' => 160, 'height' => 160], BX_RESIZE_IMAGE_PROPORTIONAL_ALT);?>
-                        <img src="<?=$arSmallFile['src']?>" alt="<?=$arItem['NAME']?>">
+                <div class="steps__item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+                    <div class="steps__img">
+                        <?php $arSmallFile = CFile::ResizeImageGet($arItem['PROPERTIES']['PICTURE']['VALUE'], ['width' => 160, 'height' => 160], BX_RESIZE_IMAGE_PROPORTIONAL_ALT); ?>
+                        <img src="<?= $arSmallFile['src'] ?>" alt="<?= $arItem['NAME'] ?>">
                     </div>
                     <div class="steps__info">
-                        <div class="steps__title title"><?=$arItem['NAME']?></div>
-                        <div class="steps__text"><?=$arItem['PREVIEW_TEXT']?></div>
+                        <div class="steps__title title"><?= $arItem['NAME'] ?></div>
+                        <div class="steps__text"><?= $arItem['PREVIEW_TEXT'] ?></div>
                     </div>
                     <div class="steps__number extrafont">
-                        <?php if($key < 9):?>0<?php endif?><?=$key + 1?>
+                        <?php if ($key < 9): ?>0<?php endif ?><?= $key + 1 ?>
                     </div>
                 </div>
-            <?php endforeach?>
+            <?php endforeach ?>
         </div>
     </div>
-<?php endif?>
+<?php endif ?>
 
 
