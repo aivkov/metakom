c87c714a130a37671aa0f473ccd9bd5e9074193a
Alexey <a343147@yandex.ru>
2026-05-27 22:51:10 +0300

    start ms123

---
+++ ms123.ru/public_html/.top.menu.php
@@ -0,0 +1,61 @@
+<?
+$aMenuLinks = Array(
+    Array(
+        "Контакты",
+        "/contacts/",
+        Array(),
+        Array(),
+        ""
+    ),
+	Array(
+		"Оплатить",
+		"https://pay.kk.bank/catalog/6101?hh",
+		Array(),
+        Array('target' => '_blank'),
+		"" 
+	),
+    Array(
+        "Оформить заявку",
+        "/order/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Услуги",
+        "/services/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Где оплатить",
+        "/payment/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Письмо руководителю",
+        "#",
+        Array(),
+        Array('modal' => 'director'),
+        ""
+    ),
+    Array(
+        "Новости",
+        "/news/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Поверка счетчиков",
+        "https://poverka-23.ru/",
+        Array(),
+        Array('target' => '_blank'),
+        ""
+    ),
+
+);
+?>
---
+++ ms123.ru/public_html/services/.left.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Домофон",
+		"/domofon/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Умный домофон",
+		"/umnyi-domofon/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Видеонаблюдение",
+		"/videonabludenie/",
+		Array(),
+		Array(),
+		""
+	),
+    Array(
+        "Ворота/шлагбаум",
+        "/gates/",
+        Array(),
+        Array(),
+        ""
+    ),
+);
+?>
