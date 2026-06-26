e426e799c8bec1e11e060638eb170e99d3d7ac71
Alexey <a343147@yandex.ru>
2026-06-26 09:28:08 +0300

    add tubes to ms123

---
+++ ms123.ru/public_html/tubes/index.php
@@ -0,0 +1,17 @@
+<?php
+/** @var \CMain $APPLICATION */
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+
+$APPLICATION->SetTitle("Типы трубок");
+$APPLICATION->SetPageProperty("title", "Типы трубок | Метаком-Новороссийск");
+$APPLICATION->SetPageProperty("description", "Типы трубок Метаком");
+?>
+
+    <div class="page">
+        <div class="container">
+            <h1 class="page__title"><?php $APPLICATION->ShowTitle(false) ?></h1>
+            <?php $APPLICATION->IncludeFile('/includes/metakom/news-detail.php', ['CODE' => 'tubes', 'TEMPLATE' => 'content']) ?>
+        </div>
+    </div>
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
